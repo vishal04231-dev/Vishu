588d43479abff52a5c417d2ba657130eecf6a364
vishal04231-dev <vishal04231@gmail.com>
2025-09-01 11:26:40 +0530

    Add files via upload

---
+++ PrimitiveDataTypesDemo.java
@@ -0,0 +1,30 @@
+ public class PrimitiveDataTypesDemo {
+    public static void main(String[] args) {
+        
+        byte b = 100;
+        
+        short s = 30000;
+
+        int i = 123456789;
+        
+        long l = 9876543210L;
+        
+        float f = 3.14f;
+        
+        double d = 3.14159265358979;
+        
+        char c = 'A';
+
+        boolean flag = true;
+        
+    
+        System.out.println("byte value: " + b);
+        System.out.println("short value: " + s);
+        System.out.println("int value: " + i);
+        System.out.println("long value: " + l);
+        System.out.println("float value: " + f);
+        System.out.println("double value: " + d);
+        System.out.println("char value: " + c);
+        System.out.println("boolean value: " + flag);
+    }
+}
