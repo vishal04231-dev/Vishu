552dc261bfe920ed53bc962508d79f33a6b9c980
vishal04231-dev <vishal04231@gmail.com>
2025-09-02 09:53:33 +0530

    Add files via upload

---
+++ SwapWithoutTemp.java
@@ -0,0 +1,18 @@
+public class SwapWithoutTemp {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        System.out.println("Before swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        num1 = num1 + num2;  
+        num2 = num1 - num2;  
+        num1 = num1 - num2;  
+
+        System.out.println("\nAfter swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+}
