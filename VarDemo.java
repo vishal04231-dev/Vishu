a93e49d8ef4b97ed3a101f9616938c82d10f0a10
vishal04231-dev <vishal04231@gmail.com>
2025-09-02 10:53:25 +0530

    Add files via upload

---
+++ VarDemo.java
@@ -0,0 +1,13 @@
+public class VarDemo {
+    public static void main(String[] args) {
+        var number = 100;
+        var price = 45.75;
+        var name = "Kanishka";
+        var isActive = true;
+
+        System.out.println("Number: " + number);
+        System.out.println("Price: " + price);
+        System.out.println("Name: " + name);
+        System.out.println("Is Active: " + isActive);
+    }
+}
