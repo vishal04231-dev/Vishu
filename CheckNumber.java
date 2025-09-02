e8792dea543194ac4bbdda92f24d089f4cc32f53
vishal04231-dev <vishal04231@gmail.com>
2025-09-02 10:06:16 +0530

    Add files via upload

---
+++ CheckNumber.java
@@ -0,0 +1,13 @@
+public class CheckNumber {
+    public static void main(String[] args) {
+        int num = -5;
+
+        if (num > 0) {
+            System.out.println(num + " is Positive");
+        } else if (num < 0) {
+            System.out.println(num + " is Negative");
+        } else {
+            System.out.println("The number is Zero");
+        }
+    }
+}
