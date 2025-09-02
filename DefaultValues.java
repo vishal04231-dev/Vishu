08a54bea58e021dec9110184f5776922cda48605
vishal04231-dev <vishal04231@gmail.com>
2025-09-02 10:08:46 +0530

    Add files via upload

---
+++ DefaultValues.java
@@ -0,0 +1,17 @@
+public class DefaultValues {
+    int a;
+    double b;
+    boolean c;
+    char d;
+    String e;
+
+    public static void main(String[] args) {
+        DefaultValues obj = new DefaultValues();
+
+        System.out.println("Default value of int: " + obj.a);
+        System.out.println("Default value of double: " + obj.b);
+        System.out.println("Default value of boolean: " + obj.c);
+        System.out.println("Default value of char: " + obj.d);
+        System.out.println("Default value of String: " + obj.e);
+    }
+}
