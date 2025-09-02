c9103d9dd43d8d2dfa3c0ab69fe2a2e688a28244
vishal04231-dev <vishal04231@gmail.com>
2025-09-02 10:32:59 +0530

    Add files via upload

---
+++ CharVsString.java
@@ -0,0 +1,17 @@
+public class CharVsString {
+    public static void main(String[] args) {
+        char ch = 'A';
+        String str = "A";
+
+        System.out.println("Character Example:");
+        System.out.println("char value: " + ch);
+        System.out.println("char occupies 2 bytes and stores a single character.");
+
+        System.out.println("\nString Example:");
+        System.out.println("String value: " + str);
+        System.out.println("String can store multiple characters (sequence of chars).");
+
+        String name = "Hello";
+        System.out.println("\nExample with multiple characters in String: " + name);
+    }
+}
