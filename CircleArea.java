bed14ab0e9d263dae0f592aceb370f958e380a44
vishal04231-dev <vishal04231@gmail.com>
2025-09-02 10:25:03 +0530

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,11 @@
+public class CircleArea {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+        double radius = 5.0;
+
+        double area = PI * radius * radius;
+
+        System.out.println("Radius of circle: " + radius);
+        System.out.println("Area of circle: " + area);
+    }
+}
