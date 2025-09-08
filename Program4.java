e96aa41294f5b84ccf50fb8c62d28027110400c6
vishal kumar <vishal04231@gmail.com>
2025-09-08 22:19:41 +0530

    Add files via upload

---
+++ Program4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CircleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = sc.nextDouble();
+
+        double area = Math.PI * radius * radius;
+
+        System.out.println("The area of the circle is: " + area);
+
+        sc.close();
+    }
+}
