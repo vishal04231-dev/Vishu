c8a2f4d9ff2550b30992fbd835be11e617ce90ba
vishal kumar <vishal04231@gmail.com>
2025-09-09 19:14:54 +0530

    Add files via upload

---
+++ KmtoMile.java
@@ -0,0 +1,8 @@
+public class KmtoMile {
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double miles = kilometers * 0.621371; // 1 km ≈ 0.621371 miles
+
+        System.out.println(kilometers + " kilometers is equal to " + miles + " miles.");
+    }
+}
