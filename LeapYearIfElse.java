efd935ba43e774a357e920c8678152ab148c50e2
vishal kumar <vishal04231@gmail.com>
2025-09-09 22:24:26 +0530

    Add files via upload

---
+++ LeapYearIfElse.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LeapYearIfElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if (year >= 1582) {
+            if (year % 400 == 0) {
+                System.out.println(year + " is a Leap Year.");
+            } else if (year % 100 == 0) {
+                System.out.println(year + " is not a Leap Year.");
+            } else if (year % 4 == 0) {
+                System.out.println(year + " is a Leap Year.");
+            } else {
+                System.out.println(year + " is not a Leap Year.");
+            }
+        } else {
+            System.out.println("Year must be >= 1582 (Gregorian calendar).");
+        }
+        sc.close();
+    }
+}
