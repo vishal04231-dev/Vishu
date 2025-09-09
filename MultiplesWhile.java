4b57a7b5a11a4da1995ad3af4d69a428c9094e70
vishal kumar <vishal04231@gmail.com>
2025-09-09 22:25:45 +0530

    Add files via upload

---
+++ MultiplesWhile.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MultiplesWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number less than 100: ");
+        int number = sc.nextInt();
+
+        if (number > 0 && number < 100) {
+            System.out.println("Multiples of " + number + " below 100:");
+            int counter = 100;
+            while (counter >= 1) {
+                if (counter % number == 0) {
+                    System.out.println(counter);
+                }
+                counter--;
+            }
+        } else {
+            System.out.println("Enter a positive number less than 100.");
+        }
+        sc.close();
+    }
+}
