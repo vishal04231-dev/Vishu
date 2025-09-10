7ed6c264987c70e65b807697e92dd122a2d1ef9d
vishal kumar <vishal04231@gmail.com>
2025-09-10 22:28:30 +0530

    Add files via upload

---
+++ EmployeeBonus.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class EmployeeBonus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] salary = new double[10];
+        double[] years = new double[10];
+        double[] bonus = new double[10];
+        double[] newSalary = new double[10];
+
+        double totalBonus = 0, totalOldSalary = 0, totalNewSalary = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter salary for employee " + (i + 1) + ": ");
+            salary[i] = sc.nextDouble();
+            System.out.print("Enter years of service for employee " + (i + 1) + ": ");
+            years[i] = sc.nextDouble();
+
+            if (salary[i] <= 0 || years[i] < 0) {
+                System.out.println("Invalid input, try again!");
+                i--;
+                continue;
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (years[i] > 5) {
+                bonus[i] = salary[i] * 0.05;
+            } else {
+                bonus[i] = salary[i] * 0.02;
+            }
+            newSalary[i] = salary[i] + bonus[i];
+            totalBonus += bonus[i];
+            totalOldSalary += salary[i];
+            totalNewSalary += newSalary[i];
+        }
+
+        System.out.println("\nTotal Bonus Payout: " + totalBonus);
+        System.out.println("Total Old Salary: " + totalOldSalary);
+        System.out.println("Total New Salary: " + totalNewSalary);
+
+        sc.close();
+    }
+}
