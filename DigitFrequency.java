c4a1e5e7cb0410f83d60742118e244e1d87b9b85
vishal kumar <vishal04231@gmail.com>
2025-09-10 22:23:06 +0530

    Add files via upload

---
+++ DigitFrequency.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class DigitFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int temp = number, count = 0;
+        while (temp != 0) {
+            count++;
+            temp /= 10;
+        }
+
+        int[] digits = new int[count];
+        int index = 0;
+        while (number != 0) {
+            digits[index++] = number % 10;
+            number /= 10;
+        }
+
+        int[] frequency = new int[10];
+        for (int i = 0; i < index; i++) {
+            frequency[digits[i]]++;
+        }
+
+        System.out.println("\nDigit Frequency:");
+        for (int i = 0; i < 10; i++) {
+            if (frequency[i] > 0) {
+                System.out.println(i + " -> " + frequency[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
